b1083ccd90178f9bd504dd2ee96a5dbb116cf27c
Aditya Kuppili <akuppili45@gmail.com>
2018-03-24 21:18:14 -0700

    test physics

--- test2.py
+++ test2.py
@@ -4,9 +4,10 @@ from pytesseract import pytesseract
 from process_image import process_image_for_ocr
 
 img = process_image_for_ocr('test2.png', IMAGE_SIZE=1800)
+img2 = process_image_for_ocr('ap-physics-c-mech-frq-2017-02.png',IMAGE_SIZE=1800)
 # img = cv2.imread(test)
 
-print(pytesseract.image_to_string(img))
+print(pytesseract.image_to_string(img2))
 # OR explicit beforehand converting
 # print(pytesseract.image_to_string(Image.fromarray(img)))
 print("hello world")
